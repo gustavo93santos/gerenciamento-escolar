2ccdfe444551735241304b01e3fe5ab856ed3ea2
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-03-26 22:52:45 -0300

    [M1S08] EX3 - Cursos

---
+++ src/main/java/br/com/fullstack/gerenciamentoescolar/model/CursoModel.java
@@ -0,0 +1,34 @@
+package com.senai.gerenciamentoalunos.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class CursoModel {
+    private static Integer proximoId = 1;
+    @Getter
+    private static List<CursoModel> cursos = new ArrayList<>();
+
+
+    @Setter(AccessLevel.NONE)
+    private Integer id;
+    private String nome;
+    private String descricao;
+    private Integer cargaHoraria;
+
+    private static Integer getProximoId(){
+        return proximoId++;
+    }
+
+    public static CursoModel addCurso(CursoModel curso){
+        curso.id = getProximoId();
+        cursos.add(curso);
+        return curso;
+    }
+}
