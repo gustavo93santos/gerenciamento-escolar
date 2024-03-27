28a088ee28895e7cf54b9d48658565b2f06df867
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-03-26 22:54:07 -0300

    [M1S08] EX6 - AlunoController

---
+++ src/main/java/br/com/fullstack/gerenciamentoescolar/controller/AlunoController.java
@@ -0,0 +1,30 @@
+package com.senai.gerenciamentoalunos.controller;
+
+import com.senai.gerenciamentoalunos.model.AlunoModel;
+import com.senai.gerenciamentoalunos.service.AlunoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("alunos")
+public class AlunoController {
+
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    @GetMapping
+    public List<AlunoModel> get() {
+        return alunoService.buscarAlunos();
+    }
+
+    @PostMapping
+    public AlunoModel post(@RequestBody AlunoModel aluno) throws Exception {
+        return alunoService.salvar(aluno);
+    }
+
+}
+
