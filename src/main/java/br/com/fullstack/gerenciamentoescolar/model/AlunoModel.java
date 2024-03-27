7eb10f7a783a36984b63e9bcb3ddb660c11c0aa5
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-03-26 22:52:28 -0300

    [M1S08] EX2 - Alunos

---
+++ src/main/java/br/com/fullstack/gerenciamentoescolar/model/AlunoModel.java
@@ -0,0 +1,34 @@
+package com.senai.gerenciamentoalunos.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class AlunoModel {
+
+    private static Integer proximoId = 1;
+    @Getter
+    private static List<AlunoModel> alunos = new ArrayList<>();
+
+    @Setter(AccessLevel.NONE)
+    private Integer id;
+    private String nome;
+    private LocalDate dataNascimento;
+
+    private static Integer getProximoId(){
+        return proximoId++;
+    }
+
+    public static AlunoModel addAluno(AlunoModel aluno){
+        aluno.id = getProximoId();
+        alunos.add(aluno);
+        return aluno;
+    }
+
+}
