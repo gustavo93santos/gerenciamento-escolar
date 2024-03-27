42f027f7626f287ade7ed1b859530cf005aba627
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-03-26 22:51:23 -0300

    [M1S08] EX1 - Projeto inicial

---
+++ src/test/java/br/com/fullstack/gerenciamentoescolar/GerenciamentoEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.gerenciamentoescolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
